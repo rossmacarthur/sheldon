9e964fe7ce36d733295bde959ea8e705ee51f2d3
Ross MacArthur <ross@macarthur.io>
2019-08-17 15:32:27 +0200

    Simplify test case orchestration

--- tests/lib.rs
+++ tests/lib.rs
@@ -1,14 +1,13 @@
 use std::{
     collections::HashMap,
-    fs, io,
+    env, fs, io,
     path::{Path, PathBuf},
     process::Command,
 };
 
-use assert_cmd::prelude::*;
-
-use handlebars::Handlebars;
-use maplit::hashmap;
+use itertools::Itertools;
+use pest::Parser;
+use pest_derive::Parser;
 
 /////////////////////////////////////////////////////////////////////////
 // Utilities
@@ -18,13 +17,108 @@ fn sheldon<R>(root: R) -> Command
 where
     R: AsRef<Path>,
 {
-    let mut command = Command::cargo_bin("sheldon").expect("`sheldon` binary in this crate");
+    // From: https://github.com/rust-lang/cargo/blob/master/tests/testsuite/support/mod.rs#L542
+    let bin = env::var_os("CARGO_BIN_PATH")
+        .map(PathBuf::from)
+        .or_else(|| {
+            env::current_exe().ok().map(|mut path| {
+                path.pop();
+                if path.ends_with("deps") {
+                    path.pop();
+                }
+                path
+            })
+        })
+        .unwrap()
+        .join("sheldon");
+
+    let mut command = Command::new(bin);
     command
         .env("HOME", root.as_ref())
         .env("SHELDON_ROOT", root.as_ref())
+        .env_remove("SHELDON_CONFIG_FILE")
+        .env_remove("SHELDON_LOCK_FILE")
         .arg("--verbose")
         .arg("--no-color");
+
     command
+}
+
+#[derive(Parser)]
+#[grammar = "../tests/case.pest"]
+struct TestCaseParser;
+
+struct TestCase {
+    root: tempfile::TempDir,
+    config_path: PathBuf,
+    lock_path: PathBuf,
+    data: HashMap<String, String>,
+}
+
+impl TestCase {
+    fn load(name: &str) -> io::Result<Self> {
+        let path = Path::new(env!("CARGO_MANIFEST_DIR"))
+            .join("tests/cases")
+            .join(name);
+        let case = &fs::read_to_string(path)?;
+        let parsed = TestCaseParser::parse(Rule::case, &case).expect("failed to parse case");
+        let root = tempfile::tempdir()?;
+        let config_path = root.path().join("plugins.toml");
+        let lock_path = root.path().join("plugins.lock");
+        let data: HashMap<String, String> = parsed
+            .filter_map(|pair| {
+                if pair.as_rule() == Rule::element {
+                    pair.into_inner()
+                        .map(|p| p.as_str().to_string())
+                        .collect_tuple()
+                        .map(|(k, v)| {
+                            (
+                                k,
+                                v.replace("<root>", root.path().to_str().unwrap())
+                                    .replace("<version>", env!("CARGO_PKG_VERSION")),
+                            )
+                        })
+                } else {
+                    None
+                }
+            })
+            .collect();
+        Ok(Self {
+            root,
+            config_path,
+            lock_path,
+            data,
+        })
+    }
+
+    fn get(&self, key: &str) -> String {
+        self.data[&key.to_string()].clone()
+    }
+
+    pub fn run_command(&self, command: &str, code: i32) -> io::Result<()> {
+        let stdout = self.get(format!("{}.stdout", command).as_str());
+        let stderr = self.get(format!("{}.stderr", command).as_str());
+        let result = sheldon(&self.root.path()).arg(command).output()?;
+        assert_eq!(result.status.code().unwrap(), code);
+        assert_eq!(String::from_utf8_lossy(&result.stdout), stdout);
+        assert_eq!(String::from_utf8_lossy(&result.stderr), stderr);
+        Ok(())
+    }
+
+    pub fn run(&self) -> io::Result<()> {
+        fs::write(&self.config_path, self.get("plugins.toml"))?;
+
+        self.run_command("lock", 0)?;
+
+        assert_eq!(
+            &fs::read_to_string(&self.lock_path)?,
+            &self.get("plugins.lock")
+        );
+
+        self.run_command("source", 0)?;
+
+        Ok(())
+    }
 }
 
 pub fn git_status(directory: &Path) -> String {
@@ -37,108 +131,18 @@ pub fn git_status(directory: &Path) -> String {
     String::from_utf8_lossy(&output.stdout).to_string()
 }
 
-pub struct TestCase {
-    pub path: PathBuf,
-    pub root: tempfile::TempDir,
-    pub config_file: PathBuf,
-    pub lock_file: PathBuf,
-    pub handlebars: Handlebars,
-    pub data: HashMap<&'static str, String>,
-}
-
-impl TestCase {
-    pub fn new(name: &'static str) -> io::Result<Self> {
-        let path = Path::new(env!("CARGO_MANIFEST_DIR"))
-            .join("tests/cases")
-            .join(name);
-        let root = tempfile::tempdir()?;
-        let config_file = root.path().join("plugins.toml");
-        let lock_file = root.path().join("plugins.lock");
-        let data = hashmap! {
-            "version" => env!("CARGO_PKG_VERSION").into(),
-            "root" => root.path().to_string_lossy().into(),
-            "config_file" => config_file.to_string_lossy().into(),
-            "lock_file" => lock_file.to_string_lossy().into(),
-        };
-        let handlebars = Handlebars::new();
-        let case = Self {
-            path,
-            root,
-            config_file,
-            lock_file,
-            handlebars,
-            data,
-        };
-
-        // Render the plugins.toml
-        case.render_file(&case.path.join("plugins.toml"), &case.config_file);
-
-        Ok(case)
-    }
-
-    pub fn render(&self, path: &Path) -> String {
-        self.handlebars
-            .render_template(
-                &fs::read_to_string(path).expect("failed to read string from file"),
-                &self.data,
-            )
-            .expect("failed to render string")
-    }
-
-    pub fn render_file(&self, source: &Path, destination: &Path) {
-        let file = fs::File::create(destination).expect("failed to create file");
-        self.handlebars
-            .render_template_to_write(&fs::read_to_string(source).unwrap(), &self.data, file)
-            .expect("failed to render file")
-    }
-
-    pub fn stdout(&self, command: &str) -> String {
-        self.render(&self.path.join(command).with_extension("stdout"))
-    }
-
-    pub fn stderr(&self, command: &str) -> String {
-        self.render(&self.path.join(command).with_extension("stderr"))
-    }
-
-    pub fn run_command(&self, command: &str, code: i32) {
-        sheldon(&self.root.path())
-            .arg(command)
-            .assert()
-            .code(code)
-            .stdout(self.stdout(command))
-            .stderr(self.stderr(command));
-    }
-
-    pub fn run(&self) -> io::Result<()> {
-        // Lock the configuration
-        self.run_command("lock", 0);
-
-        // Check that the plugins.lock file is correct
-        assert_eq!(
-            fs::read_to_string(&self.lock_file).expect("failed to read lock file"),
-            self.render(&self.path.join("plugins.lock"))
-        );
-
-        // Source the configuration
-        self.run_command("source", 0);
-
-        Ok(())
-    }
-}
-
 /////////////////////////////////////////////////////////////////////////
 // Tests
 /////////////////////////////////////////////////////////////////////////
 
 #[test]
 fn empty() -> io::Result<()> {
-    let test_case = TestCase::new("empty")?;
-    test_case.run()
+    TestCase::load("empty")?.run()
 }
 
 #[test]
 fn git() -> io::Result<()> {
-    let case = TestCase::new("git")?;
+    let case = TestCase::load("git")?;
     case.run()?;
 
     // Check that sheldon-test was in fact downloaded.
@@ -163,7 +167,7 @@ nothing to commit, working tree clean
 
 #[test]
 fn git_branch() -> io::Result<()> {
-    let case = TestCase::new("git_branch")?;
+    let case = TestCase::load("git_branch")?;
     case.run()?;
 
     // Check that sheldon-test@feature was in fact downloaded.
@@ -189,7 +193,7 @@ nothing to commit, working tree clean
 
 #[test]
 fn git_tag() -> io::Result<()> {
-    let case = TestCase::new("git_tag")?;
+    let case = TestCase::load("git_tag")?;
     case.run()?;
 
     // Check that sheldon-test@v0.1.0 was in fact downloaded.
@@ -214,17 +218,14 @@ nothing to commit, working tree clean
 
 #[test]
 fn git_bad_url() -> io::Result<()> {
-    let case = TestCase::new("git_bad_url")?;
+    let case = TestCase::load("git_bad_url")?;
+    fs::write(&case.config_path, case.get("plugins.toml"))?;
 
-    case.run_command("lock", 1);
+    case.run_command("lock", 1)?;
+    assert!(!case.lock_path.exists());
 
-    // Check that the plugins.lock file wasn't created
-    assert!(!case.lock_file.exists());
-
-    case.run_command("source", 0);
-
-    // Check that the plugins.lock file wasn't created
-    assert!(!case.lock_file.exists());
+    case.run_command("source", 0)?;
+    assert!(!case.lock_path.exists());
 
     Ok(())
 }
