af1572aa60e97286ab4250c7f3a8c8b1aa13658f
Ross MacArthur <ross@macarthur.io>
2019-10-16 09:51:16 +0200

    Cross compile, cross test, and build more binaries

--- ci/before_deploy.sh
+++ ci/before_deploy.sh
@@ -1,18 +1,29 @@
 #!/usr/bin/env bash
 
-# This script takes care of building sheldon and packaging it for release.
+# Build sheldon and package it for release.
 
 set -ex
 
 main() {
-    local target=$(rustup target list | grep '(default)' | awk '{print $1}')
+    local src="$PWD"
+    local release="$src/dist"
+    local release_bin="$src/target/$TARGET/release/$CRATE"
 
-    if [ "$TARGET" != "$target" ]; then
-        exit 1
-    fi
+    trap 'rm -rf $release' ERR
 
-    cargo build --release
-    cp "target/release/$CRATE" "$CRATE-$TRAVIS_TAG-$TARGET"
+    cross build --target "$TARGET" --release
+
+    mkdir -p "$release/docs"
+    cp "$release_bin" "$release"
+    cp LICENSE* "$release"
+    cp README.md "$release"
+    cp docs/plugins.example.toml "$release/docs"
+
+    cd "$release"
+    tar cfz "$src/$CRATE-$TRAVIS_TAG-$TARGET.tar.gz" -- *
+    cd "$src"
+
+    rm -r "$release"
 }
 
 main
